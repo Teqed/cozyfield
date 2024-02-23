8bbb015fe9014d26ac22d5acc8af83c04b112f8e
Timothy Quilling <teqed@shatteredsky.net>
2024-02-23 01:28:04 -0500

    Refactor Grid; Refactor diffusion

--- src/main.rs
+++ src/main.rs
@@ -15,29 +15,34 @@ struct Velocity {
 #[derive(Component)]
 struct GridVelocity;
 #[derive(Clone)]
-struct SubGrid(Vec<Vec2>);
-#[derive(Clone)]
-struct Grid(Vec<SubGrid>);
-impl std::ops::Index<usize> for SubGrid {
-    type Output = Vec2;
-    fn index(&self, index: usize) -> &Self::Output {
-        &self.0[index]
-    }
+struct Grid {
+    data: Vec<Vec<f32>>,
 }
-impl std::ops::IndexMut<usize> for SubGrid {
-    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
-        &mut self.0[index]
+impl Grid {
+    fn new(width: usize, height: usize) -> Self {
+        Self { data: vec![vec![0.; width]; height] }
     }
-}
-impl std::ops::Index<usize> for Grid {
-    type Output = SubGrid;
-    fn index(&self, index: usize) -> &Self::Output {
-        &self.0[index]
-    }
-}
-impl std::ops::IndexMut<usize> for Grid {
-    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
-        &mut self.0[index]
+    fn diffuse(&mut self, diffusion_rate: f32) {
+
+        let data_length = self.data.len();
+        for x in 0..data_length {
+            self.data[x][0] *= 0.75;
+            self.data[x][data_length-1] *= 0.75;
+            self.data[0][x] *= 0.75;
+            self.data[data_length-1][x] *= 0.75;
+        }
+        for y in 1..self.data.len()-1 {
+            for x in 1..self.data[0].len()-1 {
+                let current = self.data[y][x];
+                let left = self.data[y][x-1];
+                let right = self.data[y][x+1];
+                let top = self.data[y-1][x];
+                let bottom = self.data[y+1][x];
+
+                let new_value = (left + right + top + bottom).mul_add(diffusion_rate, current) / 4.0_f32.mul_add(diffusion_rate, 1.);
+                self.data[y][x] = new_value;
+            }
+        }
     }
 }
 const X_EXTENT: f32 = 500.;
@@ -67,7 +72,7 @@ fn setup(
             ..Default::default()
         },
         Velocity {
-            speed_x: 0.1,
+            speed_x: 10.,
             speed_y: 0.,
         },
     ));
@@ -96,38 +101,40 @@ fn setup(
         },
     ));
     // Spawn 20 randomcly placed and colored dots
-    for _ in 0..20 {
-        let mut dot_transform = Transform::from_xyz(
-            (rand::random::<f32>() - 0.5) * X_EXTENT,
-            (rand::random::<f32>() - 0.5) * Y_EXTENT,
-            0.,
-        );
-        dot_transform.scale = Vec3::splat(rand::random::<f32>());
-        let dot = Mesh2dHandle(meshes.add(Circle { radius: 25. }));
-        let hue = rand::random::<f32>() * 360.;
-        let dot_color = Color::hsl(hue, 1., 0.5);
-        commands.spawn((
-            MaterialMesh2dBundle {
-                mesh: dot,
-                material: materials.add(dot_color),
-                transform: dot_transform,
-                ..Default::default()
-            },
-            Velocity {
-                speed_x: 0.,
-                speed_y: 0.,
-            },
-        ));
+    let spawn_dots = true;
+    if spawn_dots {
+        for _ in 0..10 {
+            let mut dot_transform = Transform::from_xyz(
+                (rand::random::<f32>() - 0.5) * X_EXTENT,
+                (rand::random::<f32>() - 0.5) * Y_EXTENT,
+                0.,
+            );
+            dot_transform.scale = Vec3::splat(rand::random::<f32>());
+            let dot = Mesh2dHandle(meshes.add(Circle { radius: 25. }));
+            let hue = rand::random::<f32>() * 360.;
+            let dot_color = Color::hsl(hue, 1., 0.5);
+            commands.spawn((
+                MaterialMesh2dBundle {
+                    mesh: dot,
+                    material: materials.add(dot_color),
+                    transform: dot_transform,
+                    ..Default::default()
+                },
+                Velocity {
+                    speed_x: 0.,
+                    speed_y: 0.,
+                },
+            ));
+        }
     }
     commands.spawn(System::new());
 }
 #[derive(Component)]
 struct System {
-    peaks: Vec<Peak>,               // Positions of the centers of divergence
-                               // and their amplitudes
-    grid: Grid,                // 3D array to store the combined Gaussian influences
+    peaks: Vec<Peak>,
+    grid: Grid,
     resolution: Resolution,    // Defines the resolution of the grid (width, height)
-    cell_size: f32,            // The physical size of each cell in the grid
+    cell_size: f32,            // The size of each cell in the grid
 }
 struct Peak {
     grid_x: usize,
@@ -144,7 +151,7 @@ impl System {
     fn new() -> Self {
         Self {
             peaks: Vec::new(),
-            grid: Grid(vec![SubGrid(vec![Vec2 { x: 0., y: 0. }; DIMENSION]); DIMENSION]),
+            grid: Grid::new(DIMENSION, DIMENSION),
             resolution: Resolution {
                 width: DIMENSION,
                 height: DIMENSION,
@@ -176,47 +183,6 @@ impl System {
     }
     fn clear_peaks(&mut self) {
         self.peaks.clear();
-        // self.grid = Grid(vec![SubGrid(vec![Vec2 { x: 0., y: 0. }; 100]); 100]);        
-        // Have the boundaries absorb the heat
-        // The heat equation is a partial differential equation which describes the flow of heat in a given domain. For a 2D domain, it is given by the following equation:
-        // ∂u/∂t = α(∂²u/∂x² + ∂²u/∂y²)
-        // where u is the temperature, t is the time, x and y are the spatial coordinates, and α is the thermal diffusivity.
-        // The equation is solved numerically using the finite difference method, which consists in discretizing the domain into a grid and approximating the derivatives by finite differences. The discretization scheme used here is the Crank-Nicolson method, which is unconditionally stable and second-order accurate in time and space.
-        // The resulting system of equations is then solved using the Conjugate Gradient method.
-        for _ in 0..10 {
-            for x in 0..self.resolution.width {
-                self.grid[0][x] = Vec2::new(-0.1, -0.1);
-                self.grid[self.resolution.height - 1][x] = Vec2::new(-0.1, -0.1);
-            }
-            for y in 0..self.resolution.height {
-                self.grid[y][0] = Vec2::new(-0.1, -0.1);
-                self.grid[y][self.resolution.width - 1] = Vec2::new(-0.1, -0.1);
-            }
-            let thermal_diffusivity = 0.1; // Thermal diffusivity
-            let dt = 2.;   // Time step
-            let dx = 1_f32; // Spatial step
-            let dy = 1_f32; // Spatial step
-            let alpha = thermal_diffusivity * dt / (dx * dy);
-            let mut new_grid = self.grid.clone();
-            for x in 1..self.resolution.width - 1 {
-                for y in 1..self.resolution.height - 1 {
-                    let laplacian = (self.grid[y][x + 1] + self.grid[y][x - 1] - 2. * self.grid[y][x])
-                        / dx.powi(2)
-                        + (self.grid[y + 1][x] + self.grid[y - 1][x] - 2. * self.grid[y][x])
-                            / dy.powi(2);
-                    new_grid[y][x] = self.grid[y][x] + alpha * laplacian;
-                }
-            }
-            self.grid = new_grid;
-        }
-        // Cool down the grid
-        for x in 0..self.resolution.width {
-            for y in 0..self.resolution.height {
-                if self.grid[y][x].length() > 0. {
-                    self.grid[y][x] *= 0.99;
-                }
-            }
-        }
     }
     #[allow(
         clippy::cast_possible_truncation,
@@ -228,7 +194,12 @@ impl System {
             // Find the grid cell that contains the peak
             let amplitude = peak.amplitude;
             // Deposit the peak's influence into the grid cell
-            self.grid[peak.grid_y][peak.grid_x] += vec2(amplitude, amplitude);
+            self.grid.data[peak.grid_y][peak.grid_x] += amplitude * 10.;
+        }
+    }
+    fn diffuse(&mut self, steps: usize, diffusion_rate: f32) {
+        for _ in 0..steps {
+            self.grid.diffuse(diffusion_rate);
         }
     }
 }
@@ -252,36 +223,36 @@ fn move_shapes(
             entity.0.translation.y += entity.1.speed_y;
             continue;
         }
-        let left_influence = system.single().grid[cell_y][cell_x - 1];
-        let right_influence = system.single().grid[cell_y][cell_x + 1];
-        let up_influence = system.single().grid[cell_y - 1][cell_x];
-        let down_influence = system.single().grid[cell_y + 1][cell_x];
-        delta.x = (right_influence - left_influence).x;
-        delta.y = (down_influence - up_influence).y;
-        let mass = entity.0.scale.x;
+        let left_influence = system.single().grid.data[cell_y][cell_x - 1];
+        let right_influence = system.single().grid.data[cell_y][cell_x + 1];
+        let up_influence = system.single().grid.data[cell_y - 1][cell_x];
+        let down_influence = system.single().grid.data[cell_y + 1][cell_x];
+        delta.x = right_influence - left_influence;
+        delta.y = down_influence - up_influence;
+        let mass = entity.0.scale.x  * 0.1;
         entity.1.speed_x += delta.x / mass;
         entity.1.speed_y += delta.y / mass;
         // Apply a slight damping to the speed
-        entity.1.speed_x *= 0.99999;
-        entity.1.speed_y *= 0.99999;
+        // entity.1.speed_x *= 0.99999;
+        // entity.1.speed_y *= 0.99999;
         entity.0.translation.x += entity.1.speed_x;
         entity.0.translation.y += entity.1.speed_y;
-        if entity.0.translation.x > X_EXTENT - 10. || entity.0.translation.x < -X_EXTENT + 10. {
-            entity.1.speed_x = -entity.1.speed_x * 0.8;
-        }
-        if entity.0.translation.y > Y_EXTENT - 10. || entity.0.translation.y < -Y_EXTENT + 10. {
-            entity.1.speed_y = -entity.1.speed_y * 0.8;
-        }
-        if entity.0.translation.x >= X_EXTENT {
-            entity.0.translation.x = X_EXTENT - 1.;
-        } else if entity.0.translation.x <= -X_EXTENT {
-            entity.0.translation.x = -X_EXTENT + 1.;
-        }
-        if entity.0.translation.y >= Y_EXTENT {
-            entity.0.translation.y = Y_EXTENT - 1.;
-        } else if entity.0.translation.y <= -Y_EXTENT {
-            entity.0.translation.y = -Y_EXTENT + 1.;
-        }
+        // if entity.0.translation.x > X_EXTENT - 10. || entity.0.translation.x < -X_EXTENT + 10. {
+        //     entity.1.speed_x = -entity.1.speed_x * 0.8;
+        // }
+        // if entity.0.translation.y > Y_EXTENT - 10. || entity.0.translation.y < -Y_EXTENT + 10. {
+        //     entity.1.speed_y = -entity.1.speed_y * 0.8;
+        // }
+        // if entity.0.translation.x >= X_EXTENT {
+        //     entity.0.translation.x = X_EXTENT - 1.;
+        // } else if entity.0.translation.x <= -X_EXTENT {
+        //     entity.0.translation.x = -X_EXTENT + 1.;
+        // }
+        // if entity.0.translation.y >= Y_EXTENT {
+        //     entity.0.translation.y = Y_EXTENT - 1.;
+        // } else if entity.0.translation.y <= -Y_EXTENT {
+        //     entity.0.translation.y = -Y_EXTENT + 1.;
+        // }
     }
 }
 #[allow(clippy::needless_pass_by_value)]
@@ -297,6 +268,7 @@ fn add_peaks(
             .collect_peak(entity.0.translation.x, entity.0.translation.y, radius);
     }
     system.single_mut().compute_influence();
+    system.single_mut().diffuse(30, 0.8);
 }
 #[allow(clippy::needless_pass_by_value, clippy::cast_precision_loss)]
 fn change_dot_color(
@@ -326,15 +298,14 @@ fn change_dot_color(
                 (y as f32 - system.single().resolution.height as f32 / 2.).mul_add(system.single().cell_size, system.single().cell_size / 2.),
                 -10.,
             );
-            let grid_velocity = system.single().grid[y][x];
-            let radius = grid_velocity.length().mul_add(10., 5.);
+            let grid_velocity = system.single().grid.data[y][x];
+            let radius = grid_velocity.mul_add(10., 5.);
             let dot_radius = radius.abs().clamp(0.2, 20.);
             let dot = Mesh2dHandle(meshes.add(Circle { radius: dot_radius }));
-            let hue = (grid_velocity.y.atan2(grid_velocity.x) + std::f32::consts::PI)
-                / (2. * std::f32::consts::PI);
+            let hue = (grid_velocity + 1.) / 2.;
             let hue = hue * 360.;
             let saturation = // out of 1.0
-                1. - (grid_velocity.length() / 100.).min(0.01);
+                1. - (grid_velocity / 100.).min(0.01);
             let color = Color::hsl(hue, saturation, saturation * 0.3);
             commands.spawn((
                 MaterialMesh2dBundle {
